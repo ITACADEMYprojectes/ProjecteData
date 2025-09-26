6603be579f44cab7012f3b9628e7a4ef56a16b8f
lucanvieira <vieiralucan@gmail.com>
2025-09-26 12:48:05 +0200

    Create Pruebas de frequencia x cantidad de horas.sql

---
+++ Equip_15/Scripts/Pruebas de frequencia x cantidad de horas.sql
@@ -0,0 +1,25 @@
+SELECT  ID, sum(Absenteeism_hours) FROM Equip_15.RRHH_22092025 group by 1 order by 2 desc limit 10;
+
+SELECT sum(Absenteeism_hours) FROM Equip_15.RRHH_22092025;
+
+
+SELECT  ID, count(Absenteeism_hours) FROM Equip_15.RRHH_22092025 group by 1 order by  2 desc limit 10;
+ 
+SELECT count(Absenteeism_hours) FROM Equip_15.RRHH_22092025;
+
+SELECT ID, 
+       SUM(Absenteeism_hours) as total_hours,
+       COUNT(Absenteeism_hours) as absence_count
+FROM Equip_15.RRHH_22092025 
+GROUP BY ID 
+ORDER BY total_hours DESC 
+LIMIT 10;
+
+SELECT ID, 
+       SUM(Absenteeism_hours) as total_hours,
+       COUNT(Absenteeism_hours) as absence_count
+FROM Equip_15.RRHH_22092025 
+GROUP BY ID 
+ORDER BY absence_count DESC 
+LIMIT 10;
+

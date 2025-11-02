c772cd452171d36a85d4b8aa7be6acd5cc725cb7
BMunitiz <borja.munitiz@gmail.com>
2025-11-02 11:01:32 +0100

    SQL files removed

--- Equip_19/Scripts/Analista_marketing_27102025.sql
+++
@@ -1,5 +0,0 @@
-# Quin és el preu mitjà dels allotjaments per tipus d'allotjament a cada ciutat?
-SELECT ROUND(AVG(price),2) AS Average, room_type, city
-FROM Tourist_Accommodation_Clean
-GROUP BY room_type, city
-ORDER BY city;

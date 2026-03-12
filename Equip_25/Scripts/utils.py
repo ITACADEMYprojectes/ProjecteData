1c55d2995f5fbb93e5db61e68a7cb1340710e7b9
alice-rb <alicia.ripbau@gmail.com>
2026-03-12 10:55:09 +0100

    update

--- Equip_25/Scripts/utils.py
+++ Equip_25/Scripts/utils.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 
+ROOT = Path.cwd()
+while not (ROOT / "Data").exists():
+    ROOT = ROOT.parent
 
 # -----------------------------------------------------------
 ## GUARDAR GRÁFICOS
@@ -9,27 +12,19 @@ def guardar_grafico(fig, nombre_archivo, formato="png", dpi=300):
     '''Guarda un gráfico en la carpeta "Results/figures".
     Args:
         fig: Figura de Matplotlib a guardar.
-        nombre_dpt: Nombre del departamento (ej. "mkt"). 
-        tipo: Tipo de gráfico (ej. "boxplot").
-        info: Información adicional para el nombre del archivo (ej. "precio").   
+        nombre_archivo: Nombre del archivo sin extensión.
+        formato: Formato de la imagen (default: "png").
     '''
-    actual = Path.cwd()
-    ROOT = Path(__file__).resolve().parent.parent
+    ROOT = Path.cwd()
 
-    for parent in [actual] + list(actual.parents):
-        if parent.name == "src":
-            ROOT = parent.parent
-            break
+    while not (ROOT / "Data").exists():
+        ROOT = ROOT.parent
 
-    if ROOT is None:
-        raise FileNotFoundError(
-            "No se pudo detectar la raíz del proyecto"
-        )
-
-    ruta_figures = ROOT / "Results" / "figures"
+    ruta_figures = ROOT / "Data" / "figures"
     ruta_figures.mkdir(parents=True, exist_ok=True)
 
     ruta_archivo = ruta_figures / f"{nombre_archivo}.{formato}"
+
     fig.savefig(ruta_archivo, bbox_inches="tight", dpi=dpi)
     plt.close(fig)
 

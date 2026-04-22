5cae392be93eee76443d567d24af4827801d7656
ester-pinna <pnn.ester@gmail.com>
2026-04-22 14:16:29 +0200

    Añadido script de cleaning actualizado csv del dataset limpio, listo para la analisi

--- Equip_26/Scripts/CLEANING_20_04.sql
+++ Equip_26/Scripts/CLEANING_20_04.sql
@@ -41,6 +41,7 @@ ORDER BY apartment_id;
 -- La fecha es en formato VARCHAR
 -- ! Hay que transformar la fecha en datatype
 
+-- compruebo que la cosa que cambia es la fecha
 SELECT apartment_id, insert_date
 FROM Tourist_Accommodation
 WHERE apartment_id IN (
@@ -54,7 +55,7 @@ ORDER BY apartment_id, STR_TO_DATE(insert_date, '%d/%m/%Y'); -- cambio momentán
 
 -- ! Por analisi de marketing y analisi de clientes: Hay que eliminar las filas con apartment_id duplicados
 -- y quedarse solo con las filas con insert_date mas reciente.
-                       
+                      
 
 -- CUANTAS VARIABLES CATEGORIALES TENGO? (city y room_type)                        
 -- Miro cuantas distintas tipolojia de alojamento hay
@@ -78,21 +79,21 @@ FROM Tourist_Accommodation;
 SELECT *
 FROM Tourist_Accommodation
 WHERE apartment_id IS NULL;
--- no hay valores null en apartment_id
+-- NO hay valores null en apartment_id
 
 -- column city
 -- miro si hay valores null en city
 SELECT *
 FROM Tourist_Accommodation
 WHERE city IS NULL;
--- no hay valores null en city
+-- NO hay valores null en city
 
 -- column room_type
 -- miro si hay valores null en room_type
 SELECT *
 FROM Tourist_Accommodation
 WHERE room_type IS NULL;
--- no hay valores null en room_type
+-- NO hay valores null en room_type
 
 -- column price
 -- miro si hay valores null en price
@@ -120,11 +121,25 @@ FROM Tourist_Accommodation
 WHERE review_scores_rating IS NULL;
 -- Hay 1.327 valores null en review_scores_rating
 
--- ! Por analisi de experiencia de cliente: Hay que eliminar la filas con review_scores_rating = null
+-- Quantas de la filas con review_scores_rating=null tienen también las otras metricas de rating=null?
+SELECT
+    COUNT(*) AS total_review_null,
+    SUM(CASE WHEN review_scores_cleanliness IS NULL THEN 1 ELSE 0 END) AS null_cleanliness,
+    SUM(CASE WHEN review_scores_accuracy IS NULL THEN 1 ELSE 0 END) AS null_accuracy,
+    SUM(CASE WHEN review_scores_checkin IS NULL THEN 1 ELSE 0 END) AS null_checkin,
+    SUM(CASE WHEN review_scores_communication IS NULL THEN 1 ELSE 0 END) AS null_communication,
+    SUM(CASE WHEN review_scores_location IS NULL THEN 1 ELSE 0 END) AS null_location,
+    SUM(CASE WHEN review_scores_value IS NULL THEN 1 ELSE 0 END) AS null_value
+FROM Tourist_Accommodation
+WHERE review_scores_rating IS NULL;
+-- Las filas con review_scores_rating=null tienen también las otras metricas de rating nulas.
+/*Los valores nulos en review_scores_rating no representan datos faltantes, sino la ausencia de reseñas. 
+Esto se confirma porque todas las métricas de review están simultáneamente vacías en estos casos. 
+*/
 
--- Creo una vista con solo la columnas que necesito
--- Si necesito borrar la vista: DROP VIEW vista_analisi
-CREATE VIEW vista_analisi AS
+-- VALORES COHERENTES
+
+-- Verifico que las columnas que necesito tengan valores coherentes:
 SELECT
 	apartment_id,
     name,
@@ -133,17 +148,13 @@ SELECT
     price,
     review_scores_rating
 FROM Tourist_Accommodation;
-
-SELECT *
-FROM vista_analisi;
-
-
--- Verifico que las columnas que necesito tengan valores coherentes:
 -- La columna review_scores_rating presenta valores entre 600 y 1000, mientras que el rango esperado es 0-100.
 -- Esto se debe a que los valores están escalados x10.
 -- ! Para los análisis de experiencia de cliente, hay que dividir los valores entre 10
 
--- Creo copia de la tabla para hacer el cleaning:
+-- CLEANING
+
+-- Creo copia de la tabla para hacer el cleaning.
 -- Si necesito borrar la tabla: DROP TABLE copy_ta
 CREATE TABLE IF NOT EXISTS copy_ta AS
 SELECT *
@@ -176,9 +187,9 @@ WHERE review_scores_rating_clean IS NULL;
 SELECT COUNT(*)
 FROM copy_ta
 WHERE review_scores_rating IS NULL;
--- todo correcto.
+-- El numero de nulls entre las 2 columnas es igual. Todo correcto.
 
--- Borro las filas con id_apartment duplicado y tengo solo el mas reciente
+-- Borro las filas con id_apartment duplicado y tengo solo el mas reciente. Lo hago su una nueva tabla (que después borreré).
 -- Si necesito borrar la tabla: DROP TABLE copy_ta_latest
 CREATE TABLE IF NOT EXISTS copy_ta_latest AS
 SELECT *
@@ -189,18 +200,21 @@ WHERE STR_TO_DATE(insert_date, '%d/%m/%Y') = (
    WHERE t1.apartment_id = t2.apartment_id
 );
 
+-- Miro el output:
 SELECT *
 FROM copy_ta_latest
 LIMIT 10;
 
+-- Compruebo que el numero de filas borradas sea justo:
 SELECT COUNT(*) FROM copy_ta;
 -- 7001 filas totales
 -- 268 duplicados
 -- Espero encontrar 6733 filas en la nueva tabla copy_ta_latest (7001-268)
 
 SELECT COUNT(*) FROM copy_ta_latest;
--- 6733 filas, como esperado.
+-- 6733 filas, como esperado. Todo correcto.
 
+-- Tengo solo la ultima tabla con las filas borradas (ahora tenemos un id_apartment unico y no nulo)
 DROP TABLE copy_ta;
 RENAME TABLE copy_ta_latest TO copy_ta;
 
@@ -209,42 +223,124 @@ SET SQL_SAFE_UPDATES = 1;
 -- IMPUTACION
 -- Imputacion del precio
 
+-- Cuantos son los precios null?
+SELECT 
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) AS price_null,
+    ROUND(
+        SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS percentuale_null
+FROM copy_ta;
+-- los precios null son el 1.8% del total.
+
+-- Busco pattern:
+
 -- Miro si el precio falta por algun motivo
 SELECT *
 FROM copy_ta
 WHERE price IS NULL;
--- no hay nada raro, puedo continuar con la imputacion del precio.
+-- no veo nada raro, continuo con la busqueda de pattern.
 
 -- miro tambien si la falta del precio es debida al ser alojamentos no activos (has_availability=null)
 SELECT has_availability, COUNT(*) AS n
 FROM copy_ta
 WHERE price IS NULL
 GROUP BY has_availability;
--- aqui tampoco: no hay nada raro, puedo continuar con la imputacion del precio.
+-- aqui tampoco: no hay nada raro: los alojamentos con precio null no es porqué no son activos.
 
-
-SELECT city, COUNT(*) AS numero_apartment_id_null
+-- Hay una correlacion entre precio nulos i fecha?
+SELECT 
+    insert_date,
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) AS price_nulls
 FROM copy_ta
-WHERE price IS NULL
-GROUP BY city
-ORDER BY numero_apartment_id_null DESC;
--- los apartment con precio null estan concentrados en Mallorca, BCN
+GROUP BY insert_date
+ORDER BY STR_TO_DATE(insert_date, '%d/%m/%Y');
+-- Los valores nulos NO aumentan con el tiempo
 
-SELECT room_type, COUNT(*) AS numero_apartment_id_null
+-- distribución de los precios nulos por numero de accommodates:
+SELECT
+    accommodates,
+    COUNT(*) AS total_anuncios,
+    SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) AS anuncios_con_price_null,
+    ROUND(
+        SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS pct_price_null
 FROM copy_ta
-WHERE price IS NULL
-GROUP BY room_type
-ORDER BY numero_apartment_id_null DESC;
--- los apartment con precio null estan concentrados en la tipolojia de alojamento Entire home/apt
-
-SELECT accommodates, COUNT(*) AS numero_apartment_id_null
-FROM copy_ta
-WHERE price IS NULL
 GROUP BY accommodates
-ORDER BY numero_apartment_id_null DESC;
--- los apartment con precio null estan mas distribuidos respecto el numero de accomodates
+ORDER BY accommodates;
+-- Esto sugiere que los valores nulos en el precio se concentran en alojamientos de gran tamaño.
 
--- Imputacion basada en: tres variables para la imputación: city + room_type
+
+-- distribución de los precios nulos por tipo de alojamiento (room_type):
+SELECT
+    room_type,
+    COUNT(*) AS total_anuncios,
+    SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) AS anuncios_sin_precio,
+    ROUND(
+        SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS pct_price_null
+FROM copy_ta
+GROUP BY room_type
+ORDER BY pct_price_null DESC;
+-- aquì no veo ningun pattern que destaca
+
+-- distribución de los precios nulos por ciutad:
+SELECT
+    city,
+    COUNT(*) AS total_anuncios,
+    SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) AS anuncios_sin_precio,
+    ROUND(
+        SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS pct_sin_precio
+FROM copy_ta
+GROUP BY city
+ORDER BY pct_sin_precio DESC;
+-- Mallorca tiene el 5.57% de alojamentos con precio null.
+
+-- Para cada variable de review, calculo cuántos valores son NULL:
+SELECT
+    COUNT(*) AS total_price_null,
+    -- Rating
+    SUM(CASE WHEN review_scores_rating IS NULL THEN 1 ELSE 0 END) AS null_rating,
+    ROUND(SUM(CASE WHEN review_scores_rating IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS pct_null_rating,
+    -- Cleanliness
+    SUM(CASE WHEN review_scores_cleanliness IS NULL THEN 1 ELSE 0 END) AS null_cleanliness,
+    ROUND(SUM(CASE WHEN review_scores_cleanliness IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS pct_null_cleanliness,
+    -- Accuracy
+    SUM(CASE WHEN review_scores_accuracy IS NULL THEN 1 ELSE 0 END) AS null_accuracy,
+    ROUND(SUM(CASE WHEN review_scores_accuracy IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS pct_null_accuracy,
+    -- Checkin
+    SUM(CASE WHEN review_scores_checkin IS NULL THEN 1 ELSE 0 END) AS null_checkin,
+    ROUND(SUM(CASE WHEN review_scores_checkin IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS pct_null_checkin,
+    -- Communication
+    SUM(CASE WHEN review_scores_communication IS NULL THEN 1 ELSE 0 END) AS null_communication,
+    ROUND(SUM(CASE WHEN review_scores_communication IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS pct_null_communication,
+    -- Location
+    SUM(CASE WHEN review_scores_location IS NULL THEN 1 ELSE 0 END) AS null_location,
+    ROUND(SUM(CASE WHEN review_scores_location IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS pct_null_location,
+    -- Value
+    SUM(CASE WHEN review_scores_value IS NULL THEN 1 ELSE 0 END) AS null_value,
+    ROUND(SUM(CASE WHEN review_scores_value IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS pct_null_value
+FROM copy_ta
+WHERE price IS NULL;
+-- Todos los apartamentos con precio nulo que no tienen un review_scores_rating tampoco tienen ninguna de las métricas de reseñas completadas.
+-- Un 36% de los apartamentos sin precio no tienen completada la información de reseñas.
+
+-- Imputacion precio
+-- Imputacion basada en: 2 variables para la imputación: city+room_type
+
+-- Agrupo los alojamientos según: city, room_type, accomodates
+SELECT city, room_type, accommodates, COUNT(*) AS numero_observaciones
+FROM copy_ta
+WHERE price IS NOT NULL
+GROUP BY city, room_type, accommodates
+ORDER BY COUNT(*);
+-- Algunos grupos tienen pocas observaciones, no tendría sentido tomar la media.
 
 -- Agrupo los alojamientos según: city, room_type
 SELECT city, room_type, COUNT(*) AS numero_observaciones
@@ -253,7 +349,29 @@ WHERE price IS NOT NULL
 GROUP BY city, room_type
 ORDER BY COUNT(*);
 -- Algunos grupos tienen pocas observaciones, no tendría sentido tomar la media.
--- Hago una imputación por niveles.
+
+-- Cómo se distribuyen los precios dentro de cada grupo de city+room_type?
+SELECT
+    city,
+    room_type,
+    COUNT(*) AS numero_observaciones,
+    MIN(price) AS precio_minimo,
+    MAX(price) AS precio_maximo,
+    ROUND(AVG(price), 2) AS precio_medio,
+    ROUND(MAX(price) - MIN(price), 2) AS rango_precios
+FROM copy_ta
+WHERE price IS NOT NULL
+GROUP BY city, room_type
+ORDER BY numero_observaciones ASC;
+/*Algunos grupos presentan muy pocas observaciones, lo que limita la posibilidad de estimar precios representativos. 
+Además, en varios casos se observa una alta dispersión en los precios (rangos amplios entre el mínimo y el máximo), 
+lo que indica una gran variabilidad dentro del grupo. 
+Esto sugiere que el uso de la media podría no ser adecuado para la imputación en estos casos.*/
+
+-- Que hemos decidido sobre la imputacion:
+-- Hago imputacion por city y room_type (no accommodates)
+-- Dejo los precios con valor nulo si el grupo de pertenencia (city + room_type) tiene demasiadas pocas observaciones
+-- Por la imputacion tomamos la mediana (no la media)
 
 -- Primero, creo nueva columna por el precio imputado
 ALTER TABLE copy_ta
@@ -268,99 +386,126 @@ WHERE price IS NOT NULL;
 
 SET SQL_SAFE_UPDATES = 1;
 
+-- creo tabla con el numero de observaciones por cada grupo city+room_type
+CREATE TABLE group_counts AS
+SELECT 
+    city,
+    room_type,
+    COUNT(*) AS n_observaciones
+FROM copy_ta
+WHERE price IS NOT NULL
+GROUP BY city, room_type;
 
--- Imputación usando city + room_type
-UPDATE copy_ta t1
-JOIN (
-    SELECT 
+-- creo tabla con la mediana por cada grupo city+room_type
+CREATE TABLE group_medians AS
+WITH ordered_prices AS (
+    SELECT
         city,
         room_type,
-        ROUND(AVG(price), 2) AS avg_price
+        price,
+        ROW_NUMBER() OVER (
+            PARTITION BY city, room_type
+            ORDER BY price
+        ) AS rn,
+        COUNT(*) OVER (
+            PARTITION BY city, room_type
+        ) AS cnt
     FROM copy_ta
     WHERE price IS NOT NULL
-    GROUP BY city, room_type
-) t2
-ON t1.city = t2.city
-AND t1.room_type = t2.room_type
-SET t1.price_imputed = t2.avg_price
-WHERE t1.price IS NULL;
+)
+SELECT
+    city,
+    room_type,
+    ROUND(AVG(price), 2) AS median_price
+FROM ordered_prices
+WHERE rn IN (
+    FLOOR((cnt + 1) / 2),
+    FLOOR((cnt + 2) / 2)
+)
+GROUP BY city, room_type;
 
--- Imputamos los casos que siguen NULL usando solo la media por ciudad.
-UPDATE copy_ta t1
-JOIN (
-    SELECT 
-        city,
-        ROUND(AVG(price), 2) AS avg_price
-    FROM copy_ta
-    WHERE price IS NOT NULL
-    GROUP BY city
-) t2
-ON t1.city = t2.city
-SET t1.price_imputed = t2.avg_price
-WHERE t1.price IS NULL
-  AND t1.price_imputed IS NULL;
-  
--- Calculamos media global
-SET @global_price = (
-    SELECT ROUND(AVG(price), 2)
-    FROM copy_ta
-    WHERE price IS NOT NULL
-);
+-- check del output:
+SELECT * 
+FROM group_medians;
 
--- Aplicamos fallback global
-UPDATE copy_ta
-SET price_imputed = @global_price
-WHERE price IS NULL
-  AND price_imputed IS NULL;
-  
-SELECT COUNT(*)
-FROM copy_ta
-WHERE price_imputed IS NULL;
-
+-- creo tabla con la mediana de cada grupo city+room_type
+CREATE TABLE median_price_by_group AS
+SELECT 
+    c.city,
+    c.room_type,
+    c.n_observaciones,
+    m.median_price
+FROM group_counts c
+JOIN group_medians m
+    ON c.city = m.city
+   AND c.room_type = m.room_type;
+   
 SELECT *
-FROM copy_ta
-LIMIT 10;
+FROM median_price_by_group;
+
+-- creo la tabla filtrada: tengo solo los grupos city + room_type con un numero de observaciones > 10.
+CREATE TABLE median_price_valid_groups AS
+SELECT *
+FROM median_price_by_group
+WHERE n_observaciones >= 10;
+
+-- Check del output:
+SELECT *
+FROM median_price_valid_groups
+ORDER BY n_observaciones ASC, city, room_type;
+
+-- hago la imputacion de los precios:
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE copy_ta AS t
+JOIN median_price_valid_groups AS m
+    ON t.city = m.city
+   AND t.room_type = m.room_type
+SET t.price_imputed = m.median_price
+WHERE t.price IS NULL;
+
+SET SQL_SAFE_UPDATES = 1;
+
+-- Miro el output:
+SELECT
+    COUNT(*) AS total_price_null_original,
+    SUM(CASE WHEN price IS NULL THEN 1 ELSE 0 END) AS original_null,
+    SUM(CASE WHEN price IS NULL AND price_imputed IS NOT NULL THEN 1 ELSE 0 END) AS imputados,
+    SUM(CASE WHEN price IS NULL AND price_imputed IS NULL THEN 1 ELSE 0 END) AS no_imputados
+FROM copy_ta;
+-- la columna price_imputed ahora tiene los precios imputados.
+
 
 -- Imputacion del review_score
--- Imputación de review_scores_rating_clean usando otras métricas
-UPDATE copy_ta
-SET review_scores_rating_clean =
-(
-    (
-        IFNULL(review_scores_cleanliness, 0) +
-        IFNULL(review_scores_accuracy, 0) +
-        IFNULL(review_scores_checkin, 0) +
-        IFNULL(review_scores_communication, 0) +
-        IFNULL(review_scores_location, 0) +
-        IFNULL(review_scores_value, 0)
-    )
-    /
-    (
-        (review_scores_cleanliness IS NOT NULL) +
-        (review_scores_accuracy IS NOT NULL) +
-        (review_scores_checkin IS NOT NULL) +
-        (review_scores_communication IS NOT NULL) +
-        (review_scores_location IS NOT NULL) +
-        (review_scores_value IS NOT NULL)
-    )
-) * 10
 
-WHERE review_scores_rating_clean IS NULL
+-- Cuantos sonos los review_scores_rating nulos? ATTENCION! Trabajo con la nueva columna review_scores_rating_clean (con review escala 100)
+SELECT 
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN review_scores_rating_clean IS NULL THEN 1 ELSE 0 END) AS review_null_clean,
+    ROUND(
+        SUM(CASE WHEN review_scores_rating_clean IS NULL THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS pct_null_clean
+FROM copy_ta;
+-- Los review_scores_rating son el 19% del total.
 
--- Solo imputamos si al menos uno de los campos tiene valor
-AND (
-    review_scores_cleanliness IS NOT NULL OR
-    review_scores_accuracy IS NOT NULL OR
-    review_scores_checkin IS NOT NULL OR
-    review_scores_communication IS NOT NULL OR
-    review_scores_location IS NOT NULL OR
-    review_scores_value IS NOT NULL
-);
-
-SELECT COUNT(*)
+SELECT
+    COUNT(*) AS total_review_null,
+    SUM(CASE WHEN number_of_reviews = 0 THEN 1 ELSE 0 END) AS n_reviews
 FROM copy_ta
 WHERE review_scores_rating_clean IS NULL;
 
-SELECT *
+
+SELECT
+    COUNT(*) AS total_review_scores_rating_clean_null,
+    SUM(CASE WHEN review_scores_cleanliness IS NULL THEN 1 ELSE 0 END) AS null_cleanliness,
+    SUM(CASE WHEN review_scores_accuracy IS NULL THEN 1 ELSE 0 END) AS null_accuracy,
+    SUM(CASE WHEN review_scores_checkin IS NULL THEN 1 ELSE 0 END) AS null_checkin,
+    SUM(CASE WHEN review_scores_communication IS NULL THEN 1 ELSE 0 END) AS null_communication,
+    SUM(CASE WHEN review_scores_location IS NULL THEN 1 ELSE 0 END) AS null_location,
+    SUM(CASE WHEN review_scores_value IS NULL THEN 1 ELSE 0 END) AS null_value
 FROM copy_ta
-LIMIT 10;
+WHERE review_scores_rating_clean IS NULL;
+/*Los valores nulos en review_scores_rating no representan datos faltantes, sino la ausencia de reseñas. 
+Esto se confirma porque todas las métricas de review están simultáneamente vacías en estos casos. 
+Por lo tanto, no se ha realizado imputación sobre estas variables.*/

3516eb6cd6e8f52559b89c414634cf2d556ed3f3
Emilio Tornos Iniesta <etornos99@gmail.com>
2026-04-27 13:48:00 +0200

    Correción código

--- Equip_28/Data/Double_Check_Dataset_2026_27_04.sql
+++ Equip_28/Data/Double_Check_Dataset_2026_27_04.sql
@@ -17,17 +17,9 @@ FROM RRHH_27042026;
 
 -- ID duplicados exactos: 
 
-SELECT *, COUNT(*) as repeticiones
-FROM RRHH_27042026
-GROUP BY ID, Reason_absence, Month_absence, Day_week, Seasons, Transportation_expense, 
-         Distance_Residence_Work, Service_time, Age, Work_load_Average_day, Hit_target, 
-         Disciplinary_failure, Education, Son, Social_drinker, Social_smoker, Pet, 
-         Weight, Height, Body_mass_index, Absenteeism_hours
-HAVING COUNT(*) > 1;
-
-SELECT SUM(repeticiones - 1) AS total_id_duplicados_exactos
+SELECT COUNT(*) AS total_id_duplicados_exactos
 FROM (
-    SELECT COUNT(*) as repeticiones
+    SELECT ID
     FROM RRHH_27042026
     GROUP BY ID, Reason_absence, Month_absence, Day_week, Seasons, Transportation_expense, 
              Distance_Residence_Work, Service_time, Age, Work_load_Average_day, Hit_target, 
